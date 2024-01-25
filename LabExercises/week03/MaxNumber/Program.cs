92d7a5b02690627fc448c830aebb54d92ae602e9
mabbasiazad <m32abbas@uwaterloo.ca>
2024-01-25 16:45:10 -0500

    max number function added

---
+++ LabExercises/week03/MaxNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace MaxNumberProgram;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter the 1st number");
+        string fristInValue = Console.ReadLine();
+
+        Console.WriteLine("enter the 2nd number");
+        string secondInValue = Console.ReadLine();
+
+        double numberA = double.Parse(fristInValue); 
+        double numberB = double.Parse(secondInValue);
+
+        double maxNumber = numberA;
+        if (numberB > numberA) maxNumber = numberB;            
+
+        Console.WriteLine($"the max number if {maxNumber}");
+        
+    }
+}
