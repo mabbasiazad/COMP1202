e8d5549aa34d21ff815fd9be977221092f4825b9
mabbasiazad <m32abbas@uwaterloo.ca>
2024-01-25 12:45:18 -0500

    week03 bonus app created

---
+++ LabExercises/week03/BonusCalc/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BonusCalcProgram; 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string inValue;
+        decimal salesForYear;
+        decimal bonusAmount = 0M;
+        Console.WriteLine("Do you get a bonus this year ?");
+        Console.WriteLine ();
+        Console.WriteLine ("To determine if you are due one,");
+        Console.Write ("enter your gross sales figure : ");
+        inValue = Console.ReadLine() ;
+        salesForYear = Convert . ToDecimal (inValue);
+
+        if (salesForYear > 500000.00M)
+        {
+            Console.WriteLine() ;
+            Console.WriteLine(" YES . . . you get a bonus !");
+            bonusAmount = 1000.00M ;
+        }
+            Console.WriteLine("Bonus for the year : {0:C}", bonusAmount);
+            Console.ReadKey(); 
+       
+
+    }
+}
