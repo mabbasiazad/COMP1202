90b51d14c7b3c47c64fdd5ec1be461c6380c8322
mabbasiazad <m32abbas@uwaterloo.ca>
2024-02-01 14:37:37 -0500

    guess number added

---
+++ LabExercises/week04/GuessRandNum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace week04;
+public class GuessRand
+{
+    public static void Main(string[] args)
+    {
+        int randNum = new Random().Next(1, 10); 
+        int userInput;
+        const int numOfGuess = 10;
+        int counter = 0;
+
+        while (counter <= numOfGuess)
+        {
+            Console.WriteLine("Guess a number between 1 to 100");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            counter ++;
+            if (userInput == randNum) {
+                Console.WriteLine($"Congradulation you won after {counter} try");
+                break;
+            }
+
+            else {
+                Console.WriteLine("Please try again");
+            }
+        }
+
+        Console.WriteLine($"the true number was {randNum}");
+    }
+
+
+}
