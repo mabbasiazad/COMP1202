f65fded068a0bfb95e9de3733cae2beb75acd998
mabbasiazad <m32abbas@uwaterloo.ca>
2024-02-23 10:25:19 -0500

    week07_fri10am

---
+++ LabExercises/Lab07_Fri10am/TestString/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string s = "COMP1202 Object-Oriented Programming";
+        //index     0123456789...
+        System.Console.WriteLine("Original String: " + s);
+
+        // 1- access by indexing
+        char letter = s[3]; 
+        System.Console.WriteLine("Character at index 3: " + letter);
+
+        // 2- substring
+        string sub_string = s[1..7];
+        System.Console.WriteLine("Substring from index 1 (included) to 7 (exluded): " + sub_string);
+
+        // 3- indexOf()
+        int index = s.IndexOf("M");
+        System.Console.WriteLine("Index of the first occurrence of 'M': " + index);
+
+        index = s.IndexOf("Object"); //????
+        System.Console.WriteLine("Index of the first occurrence of 'Object': " + index);
+
+        // 4- length of the strign
+        Console.WriteLine("Length of the string: " + s.Length);
+
+
+
+    }
+}
