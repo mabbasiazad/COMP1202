559a83668efc069be954d1ff74da1b8c5937b6bf
mabbasiazad <m32abbas@uwaterloo.ca>
2024-02-23 11:30:43 -0500

    findValidGene initiated

--- LabExercises/Lab07_Fri10am/Genome/Program.cs
+++ LabExercises/Lab07_Fri10am/Genome/Program.cs
@@ -1,4 +1,6 @@
-﻿class Genomics
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Genomics
 {
     public string findGene(string dna){
         //Start codon: ATG
@@ -24,6 +26,19 @@
         //report substring between ATG and TAA (codon included)
         return dna[index_atg..(index_taa + 3)];
     }
+
+    public string findValidGene(string dna){
+        int startIndex = dna.IndexOf("ATG");
+        if(startIndex == -1){
+            return "No gene found";
+        }
+
+        int stopIndex = dna.IndexOf("TAA", startIndex + 3);
+
+        //in valid gene stopIndex - startIndex should be multiple of 3
+        //if stopIndex - startIndex is not multiple of 3, find the next TAA
+        //Do this until you find a valid gene or stopIndex is -1
+    }
 }
 
 class Program
@@ -43,6 +58,9 @@ class Program
         sub_gene = gene.findGene(dna2);
         Console.WriteLine("The gene section: " + sub_gene);
         
+        string dna3 = "ATGxxxyyyzzzTAA";
+
+        string dna4 = "ATGxxxyyyzzzyyTAAxxxyyyzzzTAA";
         
     }
 }
