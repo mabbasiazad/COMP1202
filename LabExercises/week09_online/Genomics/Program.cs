a71ac76723290177570ae782f52ed1f4a3c4caef
mabbasiazad <m32abbas@uwaterloo.ca>
2024-03-05 16:19:06 -0500

    findGene implemented

--- LabExercises/week09_online/Genomics/Program.cs
+++ LabExercises/week09_online/Genomics/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+class Genomics
+{
+    public string findGene(string dna) {
+        //your code here
+        //1- I find the first occurence of ATG
+        int start = dna.IndexOf("ATG");
+        if(start == -1)
+        {
+            return "";
+        }
+        //2- I find the occurance of TAA
+        int stop = dna.IndexOf("TAA", start + 3);
+        if (stop == -1)
+        {
+            return "";
+        }
+        //3- I will report all letters between ATG index and TAA index
+        return dna[start..(stop + 3)];
+
+    }
+
+    public static void Main(string[] args)
+    {
+        //ATG is start codeon
+        //TAA is stop codeon
+        string dna = "CTATGATCGGGCCTAACT";
+        //              ^            ^
+    }
+}
---
+++ LabExercises/week09_online/WarmUp/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string s = "This is COMP1202 class";
+        //index     0123456789 
+        //1) Q: I want to get access to the 10th character of this string
+        char ch = s[9]; 
+        Console.WriteLine("The 10th character is: " + ch);
+
+        //2) Q: I want the characters between 1st to 6th characters
+        // first index is inclusive but the second index is exclusive 
+
+        string sub_string = s[0..6];
+        Console.WriteLine("The sub string is: " + sub_string);
+
+        // 3) what is the index of 
+        int index = s.IndexOf("M");
+        Console.WriteLine("The index of M is: " + index);
+
+        index = s.IndexOf("COMP");
+        Console.WriteLine("The index of COMP is: " + index);
+
+        index = s.IndexOf("W");
+        Console.WriteLine("The index of W is: " + index);
+
+        index = s.IndexOf("O", 11);
+        
+
+
+
+    }
+}
