ea5afd82884055d7b368afd5584aca033359e820
mabbasiazad <m32abbas@uwaterloo.ca>
2024-03-06 09:30:47 -0500

    validgene created

--- LabExercises/week09_online/Genomics/Program.cs
+++ LabExercises/week09_online/Genomics/Program.cs
@@ -2,7 +2,7 @@
 
 class Genomics
 {
-    public string findGene(string dna) {
+    public static string findGene(string dna) {
         //your code here
         //1- I find the first occurence of ATG
         int start = dna.IndexOf("ATG");
@@ -21,11 +21,59 @@ class Genomics
 
     }
 
+    public string findValidGene(string dna) {
+        //excercise
+        //1- find start codeone (ATG)
+        int startIndex = dna.IndexOf("ATG");  
+        
+        if(startIndex == -1)
+        {
+            return "";
+        }
+
+        int stopIndex = dna.IndexOf("TAA", startIndex);
+
+
+        while (stopIndex != -1)
+        {
+            if((stopIndex - stopIndex) % 3 == 0){
+                return dna[startIndex..(stopIndex)];
+            }
+
+            //...
+        }
+
+        //LOOP (stop != -1) {
+        //2- find stop codeone after ATG (stop codeon = TAA)
+
+        //3- check if the (stop - start) % 3 == 0
+            //true: report my gene dna[start..stop + 3]
+            //false: return to step 2
+        //}
+    }
+
     public static void Main(string[] args)
     {
         //ATG is start codeon
         //TAA is stop codeon
         string dna = "CTATGATCGGGCCTAACT";
         //              ^            ^
+        Genomics obj= new Genomics();
+        //string gene = obj.findGene(dna);
+        string gene = Genomics.findGene(dna);
+
+        Console.WriteLine($"The final result will be: {gene}");
+        //pass dna to findGene() method and print the gene
+
+        dna = "ATCGTATACTGAAAACAGCTTTGAGATTGTTAAACACCGAAGAGTTATGATTT";
+        //gene = obj.findGene(dna);
+        gene = Genomics.findGene(dna);
+        Console.WriteLine($"The final result will be: {gene}");
+
+        dna = "TGTATGAGCATACGACGGTCTAGAGCTGGGATGAGGCCCGAACACTAAGTTATGGGT";
+        //gene = obj.findGene(dna);
+        gene = Genomics.findGene(dna);
+        Console.WriteLine($"The final result will be: {gene}");
+
     }
 }
--- LabExercises/week09_online/Genomics/obj/Debug/net8.0/Genomics.AssemblyInfo.cs
+++ LabExercises/week09_online/Genomics/obj/Debug/net8.0/Genomics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Genomics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f8b9de0cbecb3a600b464e6a18fcb4cb631857")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71ac76723290177570ae782f52ed1f4a3c4caef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Genomics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Genomics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
