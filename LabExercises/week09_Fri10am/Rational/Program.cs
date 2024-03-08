1a12982102078867bb2209ffda57b168d51cd402
mabbasiazad <m32abbas@uwaterloo.ca>
2024-03-08 10:47:40 -0500

    class created and tested

--- LabExercises/week09_Fri10am/Rational/Program.cs
+++ LabExercises/week09_Fri10am/Rational/Program.cs
@@ -1,7 +1,23 @@
-﻿class Rational
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Xml.Schema;
+
+class Rational
 {
     public int numer; 
-    public int denom; 
+    private int denom; 
+
+    public int Denom {
+        set {
+            denom = value;
+        }
+
+        get {
+            return denom;
+        }
+    }
+
+
 
     //default construcor
     public Rational() {
@@ -17,6 +33,9 @@
     //override ToString() method
     public override string ToString(){
         //show your desired message when outputing / printing the object of class Rational
+        string msg = $"{numer}/{denom}";
+
+        return msg; 
 
     }
 }
@@ -28,7 +47,16 @@ class Program
         // create an object or instance from my class
         // declare / assignment / intializae/ ... 
         Rational r1 = new Rational(x:2, y:3); 
-        Console.WriteLine("my rational number is: " + r1.ToString());
+        Console.WriteLine("the first rational number is: " + r1.ToString());
+
+        //type(classname) nameforvarialble = new calltheconstructor
+        Rational r2 = new Rational();
+        // . (dot)is my access operator to varialbes class
+        r2.Denom = 5;
+        r2.numer = 8; 
+        Console.WriteLine("the second rational number is: " + r2);
+
         
+
     }
 }
--- LabExercises/week09_Fri10am/Rational/obj/Debug/net8.0/Rational.AssemblyInfo.cs
+++ LabExercises/week09_Fri10am/Rational/obj/Debug/net8.0/Rational.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rational")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b5c1b1abd704b1a90a012f40fdd69a49907787")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+019aedc27bd925663a041a66023dc76603f4750f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rational")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rational")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
