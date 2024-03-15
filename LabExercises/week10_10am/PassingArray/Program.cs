33377e031f278944e4cb061bd2288034cc76ca85
mabbasiazad <m32abbas@uwaterloo.ca>
2024-03-15 11:21:00 -0400

    arraypassing

---
+++ LabExercises/week10_10am/PassingArray/Program.cs
@@ -0,0 +1,41 @@
+﻿﻿using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    public static void arrayChange(int[] inputArray){
+        // change the 0th element of the inputArray
+        inputArray[0] = 1000;
+    }
+
+    public static void numberChange(int input){
+        //change input
+        input = 3000;
+    }
+
+    public static void printArray(int[] inputs){
+        foreach (var item in inputs)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+    public static void Main(string[] args)
+    {
+        int[] numbers = new int[10]; 
+        numbers = [2, 4, 13, 12, 11, 9, 25, 2, 64,99];
+        //pass numbers to average method and get the average and display it
+
+        Console.WriteLine("numbers before arrayChange call");
+        printArray(numbers);
+
+        arrayChange(numbers);
+        Console.WriteLine("numbers after arrayChange call");
+        printArray(numbers);
+
+
+        int number = 10; 
+        Console.WriteLine("number before numberChange call: " + number);
+        numberChange(number); 
+        Console.WriteLine("number after numberChange call: " + number);        
+    }
+}
--- LabExercises/week10_10am/RandomArray/Program.cs
+++ LabExercises/week10_10am/RandomArray/Program.cs
@@ -1,7 +1,38 @@
 ﻿class Program
 {
+    public double average(int[] array) {
+        //your code here : sum / count
+        int sum = 0;
+        foreach (var item in array)
+        {
+            sum = sum + item;
+            // sum += item
+        }
+
+        return (double) sum / array.Length;
+
+    }
     public static void Main(string[] args)
     {
         //create an array of size 10 of random numbers between 0, 100
+        Random rand = new Random();  //creat an object of rand
+        
+        int[] randArray = new int[10];
+
+        for (int i = 0; i < randArray.Length; i++)
+        {
+            randArray[i] = rand.Next(0, 100); 
+            Console.Write(randArray[i] + " ");    
+        }
+
+        randArray = new int[]{12, 8, 7, 6, 27, 81, 92, 100};
+        
+        // this is for non-static average
+        Program obj = new Program();
+        double ave = obj.average(randArray);
+        //ave = 41.6
+        Console.WriteLine();
+        Console.WriteLine("the average of the elements in the array is: " + ave);
+
     }
 }
