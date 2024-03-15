5829e203e8a56e3d671bd6a4cf7346b5e115c8ec
mabbasiazad <m32abbas@uwaterloo.ca>
2024-03-15 09:14:45 -0400

    update

---
+++ LabExercises/week10_Fri8am/Average/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public double average(???) {
+
+    }
+    public static void Main(string[] args)
+    {
+        int[] numbers = new int[10]; 
+        numbers = [2, 4, 13, 12, 11, 9, 25, 2, 64,99];
+        //pass numbers to average method and get the average and display it
+
+        int[] numbers_ = {2, 4, 13, 12, 11, 9, 25, 2, 64,99};
+    }
+}
--- LabExercises/week10_Fri8am/RandomNumberArray/Program.cs
+++ LabExercises/week10_Fri8am/RandomNumberArray/Program.cs
@@ -3,6 +3,22 @@
     public static void Main(string[] args)
     {
         //create an array of random numbers (between 0, 100) of size = 10
-        
+        Random rand = new Random();
+        int a = rand.Next(0, 100); 
+        Console.WriteLine(a);
+
+        //array of size 10
+        int[] array;   //declare
+        array = new int[10];  //create
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(0, 100);
+            Console.Write(array[i] + " ");           
+        }
+
+
+
+
     }
 }
