c569fab424f2e86e27b3285338796b0b38f8ba3f
mabbasiazad <m32abbas@uwaterloo.ca>
2024-03-15 10:14:50 -0400

    initial

---
+++ LabExercises/week10_10am/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //exercise 1: create this array and display it in the console
+        //[12, 8, 7, 6, 27, 81, 92, 100]
+
+        //1.declare
+        int a; 
+        //2. initialize / assign
+        a = 12; 
+        
+        //1.
+
+        //2.
+        //3.
+    }
+}
