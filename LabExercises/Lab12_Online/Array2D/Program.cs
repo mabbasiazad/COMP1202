38373bba546967163300d90a5ab8b42a17d4571d
mabbasiazad <m32abbas@uwaterloo.ca>
2024-03-26 15:43:44 -0400

    initial commit

---
+++ LabExercises/Lab12_Online/Array2D/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //names = {"Ali", "Sara", "Ben"}
+        //names: Ali    Sara    Ben
+        string[] names = new string[3]{"Ali", "Sara", "Ben"};
+        Console.Write("names: ");
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.Write($"{names[i]}\t");
+        }
+
+        //grades = 86   90  45  68
+        //         79   87  85  94
+        //         80   75  50  75 
+        int[,] grades = new int[3,4]{{86, 90, 45, 68} , {79, 87, 85, 94}, {80, 75, 50, 75}};
+        //access operator
+        int sample = grades[1, 3];
+        Console.WriteLine();
+        Console.WriteLine("the sample element in i = 1 ; j = 3 : " + sample);
+
+        int n_rows = grades.GetLength(0);   // n_rows = 3
+        int n_cols = grades.GetLength(1);   // n_cols = 4
+
+        //loop over the rows of the 2D array
+        for (int i = 0; i < n_rows; i++)
+        {
+            Console.Write("name: " + names[i] + "\t");
+            Console.Write("Grades: ");
+            // loop over the columns when the row is fixed
+            for (int j = 0; j < n_cols; j++)
+            {
+                Console.Write($"{grades[i,j]}" + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+
+
+
+
+    }
+}
---
+++ LabExercises/week11_10am/Array2D/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] names = new string[2] {"Mehdi", "Alex"};
+
+        double[,] grades = new double[2,4] {{70, 80, 85, 40}, {85, 90, 20, 45}};
+
+        Console.WriteLine("sample elemets of the 2D array:" + grades[1,1]);
+
+        // for (int i = 0; i < names.Length; i++)
+        // {
+        //     Console.Write(names[i] + " ");
+        // }
+
+        // Console.WriteLine();
+
+        int n_rows = grades.GetLength(0);
+        int n_cols = grades.GetLength(1);
+
+        int length = grades.Length;
+
+        Console.WriteLine(grades.GetLength(2));
+
+        // double sum;
+        // double count = n_cols;
+
+        // for (int i = 0; i < n_rows; i++)
+        // {
+        //     sum = 0;
+        //     Console.Write($"Name: {names[i]}\t");
+        //     Console.Write("Grades: ");
+        //     for (int j = 0; j < n_cols; j++)
+        //     {
+        //         Console.Write(grades[i,j] + " ");
+        //         sum = sum + grades[i, j];
+        //     }
+
+        //     double ave = sum / count;
+        //     Console.Write($"\tave: {ave}");
+
+        //     Console.WriteLine();
+        // }
+
+    }
+}
