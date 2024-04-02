de6db8f19bcdde671f6d7feac10ef9ef19c73954
mabbasiazad <m32abbas@uwaterloo.ca>
2024-04-02 16:47:34 -0400

    flashcard

---
+++ LabExercises/week12_online_Inclass/FlashCard2/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath = @"C:\Texts\Questions.txt";
+        //string filePath = @"C:\Texts\QuestionsDocx.docx";
+        string[] questions = new string[10];
+        string[] answers = new string[10];
+        
+
+        using(StreamReader read = new StreamReader(filePath)){
+            string line = read.ReadLine();            
+            //questionAnswer[0] (left side of the split)   questionAnswer[1] (right side of split)
+            int count = 0;
+            while (line != null)
+            {
+                string[] questionAnswer = line.Split(":");
+                questions[count] = questionAnswer[0];
+                answers[count] = questionAnswer[1];
+                Console.WriteLine(line);
+                line = read.ReadLine(); 
+                count++;               
+            }
+        }
+
+        for (int i = 0; i < answers.Length; i++)
+        {
+            Console.WriteLine($"Question_{i}: " + questions[i]);
+            Console.WriteLine($"Answer_{i}: " + answers[i]);
+            
+        }
+        
+    }
+}
---
+++ LabExercises/week12_online_Inclass/ReadWrite/Program.cs
@@ -0,0 +1,33 @@
+﻿//StreamReader  class to read the text file
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // I have a text file "C:\Texts\sampletext.txt" I want to the lines of this file
+        //StreamReader class to do the job for me\
+        //obj.ReadLine()
+        // line == null line == ""
+
+        string fileName = @"C:\Texts\sampletext.txt";
+        StreamReader read = new StreamReader(fileName);
+
+        string line = read.ReadLine(); 
+        while (line != null) {
+            Console.WriteLine(line);
+            line = read.ReadLine();
+        }
+
+        read.Close(); //free 
+
+        using (StreamReader read_ = new StreamReader(fileName))
+        {
+            string line_ = read.ReadLine(); 
+            while (line_ != null) {
+                Console.WriteLine(line_);
+                line_ = read.ReadLine();
+            }
+        }    
+
+            
+    }    
+}
