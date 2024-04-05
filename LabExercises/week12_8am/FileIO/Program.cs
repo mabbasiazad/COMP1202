49b48887f28da3ab04a36ff2538ed02fb0bddc58
mabbasiazad <m32abbas@uwaterloo.ca>
2024-04-05 09:49:15 -0400

    week 12 8am class

---
+++ LabExercises/week12_8am/FileIO/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+
+public static void Main(string[] args)
+{
+    string directoryPath = @"C:\Animals";
+    //string directoryPath = @"C:\Users\mabba\OneDrive - George Brown College\Winter2024\COMP1202\LabExercises\week12_online\Texts";
+    //read this folder/directory and list all files inside it
+    // dog.jpg ....
+    // File or Directory class 
+    // FileInfo DirectoryInfo class
+    /*
+    string[] files = Directory.GetFiles(directoryPath); 
+
+    foreach (var file in files)
+    {
+        Console.WriteLine(file);
+    }
+    */
+
+    // DirectoryInfo class  // FileInfo[]
+    //display the name of the directory (obj.Name)
+    //display the file names (obj.GetFiles())  -- file.Name)
+    DirectoryInfo directoryObj = new DirectoryInfo(directoryPath); 
+
+    Console.WriteLine("Directory Name" + directoryObj.Name);
+    FileInfo[] files = directoryObj.GetFiles(); 
+
+    foreach (var file in files)
+
+    {
+        string fileName = "File Name: " + file.Name;
+        string fileSize = "Size: " + file.Length;
+        DateTime creationTime = file.CreationTime;
+        //showing the info in aligned format            
+        Console.WriteLine(fileName.PadRight(30) + fileSize.PadRight(20) + creationTime);   
+    }
+}
+
+    
+}
---
+++ LabExercises/week12_8am/ReadingWriting/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //try to read Qestions.txt file and display the file line by line
+        //use StreamReader class for this purpose
+        // obj.ReadLine() method
+        string filePath = @"C:\Texts\Questions.txt";
+
+        
+        /*
+        By what initials was Franklin Roosevelt better known?:FDR
+        Which number president was Franklin Roosevelt?:32
+        Which state was Franklin Roosevelt born in?:New York
+        In which year did Roosevelt become Governor of New York?:1929
+        What was the name of Franklin Roosevelt's wife?:Eleanor
+        How many children did Franklin Roosevelt have?:6
+        From which university did Franklin Roosevelt graduate with an A.B in history?:Harvard
+        What was the first name of Franklin Roosevelt's 5th cousin, who was also President?:Theodore
+        Which disease is believed to be the causes of Franklin Roosevelt's paralysis?:Polio
+        At what age did Franklin Roosevelt die?:63
+        */
+
+        // StreamReader strObj =  new StreamReader(filePath);
+        // string line = ""; 
+        // line = strObj.ReadLine();
+        // while (line != null){
+        //     Console.WriteLine(line);
+        //     line = strObj.ReadLine();
+        // }
+        
+        // strObj.Close();
+
+        // using (StreamReader strObj_ =  new StreamReader(filePath)) {
+        //     string line_ = ""; 
+        //     line_ = strObj.ReadLine();
+        //     while (line != null){
+        //         Console.WriteLine(line_);
+        //         line = strObj.ReadLine();
+        // }
+
+        string[] questions = new string[10]; 
+        string[] answers = new string[10]; 
+
+        using(StreamReader strObj_2 =  new StreamReader(filePath)) {
+            string l = "";
+            //reading from my file
+            l = strObj_2.ReadLine();
+            int count = 0;
+            while(l != null){                
+                // processing the data
+                string[] questionAnswer = l.Split(":");
+                questions[count] = questionAnswer[0];
+                answers[count] = questionAnswer[1];
+                count++;
+                l = strObj_2.ReadLine();
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Question_{i}: " + questions[i]);
+                Console.WriteLine($"answer_{i}: " + answers[i]);
+            }
+                
+
+
+        }   
+
+        // read all lines and create an array of questons and answers     
+    }
+}
