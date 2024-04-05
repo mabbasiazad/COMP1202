9d3b3e8018a0de2ddfd98bbd43aa9aa46c342f2f
mabbasiazad <m32abbas@uwaterloo.ca>
2024-04-05 10:47:52 -0400

    directory and directoryInfo

---
+++ LabExercises/week12_10am/FileIO/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //exercise: list all the files of the folder/directory
+        // dog.jpg ... 
+        string path = @"C:\Animals";
+
+        // Directory class
+        // DirectoryInfo + FileInfo classes
+
+        string[] files = Directory.GetFiles(path);
+        foreach (string file in files)
+        {
+            Console.WriteLine("File Name: " + file);   
+        }
+
+        //DirectoryInfo class + FileInfo class- the methods are non-static
+        // creat obj === obj.GetFiles()
+        DirectoryInfo dirObj = new DirectoryInfo(path);
+        FileInfo[] files_= dirObj.GetFiles();
+
+        foreach (FileInfo file in files_)
+        {
+            string fileName = "File Name: " + file.Name;
+            string fileSize = "Size: " + file.Length;
+            DateTime creationTime = file.CreationTime;
+            //showing the info in aligned format            
+            Console.WriteLine(fileName.PadRight(30) + fileSize.PadRight(20) + creationTime);  
+        }
+    }
+}
---
+++ LabExercises/week12_10am/ReadWrite/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath = @"C:\Texts\Questions.txt";
+        //exercise read the context of Questions.txt line by line
+        //hint: StreamReader class 
+    }
+}
