87e98ee9115846d607bf45a984213a930e193266
mabbasiazad <m32abbas@uwaterloo.ca>
2024-04-11 10:44:16 -0400

    max of all days

--- LabExercises/Week13_Online/Export/Export/obj/Debug/net8.0/Export.AssemblyInfo.cs
+++ LabExercises/Week13_Online/Export/Export/obj/Debug/net8.0/Export.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Export")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d4843464cd2867bfd62352abb251ee6469d15e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd447fcf015f788f5acf999e05bb370d79441d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Export")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Export")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LabExercises/Week13_Online/Export/MaxTemp/Program.cs
+++ LabExercises/Week13_Online/Export/MaxTemp/Program.cs
@@ -1,8 +1,11 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class MaxTemp{
     
-    public static void hottestTimeOfOneDay(string filePath){
+    public static string[] hottestTimeOfOneDay(string filePath){
+        string fileName = Path.GetFileName(filePath);
+        string[] output = null;
         using(StreamReader reader = new StreamReader(filePath)){
             string line = null;  
             line = reader.ReadLine(); // Skip the first line
@@ -27,14 +30,52 @@ class MaxTemp{
                 }
             }
 
-            Console.WriteLine("Max Temp: " + MaxTemp + " at " + MaxTime);
-        }        
+            // Console.Write("Date: " + fileName + " "); 
+            // Console.WriteLine("Max Temp: " + MaxTemp + " at " + MaxTime);
+
+            output = new string[] {MaxTemp, fileName, MaxTime};
+            return output;
+        }    
+
+           
     }
     public static void Main(string[] args)
     {
         string filePath = @"C:\Users\mabba\OneDrive - George Brown College\Winter2024\COMP1202\LabExercises\Week13_Online\weather-2015-01-01.csv";
-        hottestTimeOfOneDay(filePath); 
+        //hottestTimeOfOneDay(filePath); 
 
-        //exercise: write a method to find the max temp of the year
+        string directoryPath = @"C:\Users\mabba\OneDrive - George Brown College\Winter2024\COMP1202\LabExercises\Week13_Online\2015";
+        string[] files = Directory.GetFiles(directoryPath);
+
+        string[] tempDateTime = null; 
+        // tempDateTime = hottestTimeOfOneDay(files[0]);
+        // Console.Write("MaxTemp: " + tempDateTime[0] + "\t"); 
+        // Console.WriteLine("Date: " + tempDateTime[1] + "\t" + "Time: " + tempDateTime[2]);
+
+        List<Double> temps = new List<Double>();
+        List<String> dates = new List<String>(); 
+        List<String> times = new List<String>();
+
+        foreach (string file in files)
+        {
+            tempDateTime = hottestTimeOfOneDay(file);
+            temps.Add(double.Parse(tempDateTime[0])); 
+            dates.Add(tempDateTime[1]);
+            times.Add(tempDateTime[2]);
+        }
+
+        foreach (var temp in temps)
+        {
+            Console.Write(temp + " "); 
+        }
+
+        Console.WriteLine("\nMax of Maxs: " + temps.Max()); 
+
+        double MaxOfMaxs = temps.Max();
+        int index = temps.IndexOf(MaxOfMaxs); 
+
+        Console.WriteLine("\nMax of Maxs: " + MaxOfMaxs + "\t" + "Index: " + index); 
+        Console.WriteLine("Test: " + temps[index]);
+        Console.WriteLine("Date: " + dates[index] + "\t" + "Time: " + times[index]); 
     }
 }
